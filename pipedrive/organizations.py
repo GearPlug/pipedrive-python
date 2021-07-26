3adc6de74b277f5f0108b04088098cdee7cb9fac
Danie Krige <dk@bwsonline.co.za>
2021-07-26 08:15:50 +0200

    Added pull requests
    Added pull requests

--- pipedrive/organizations.py
+++ pipedrive/organizations.py
@@ -26,6 +26,11 @@ class Organizations(object):
         url = 'organizationFields'
         return self._client._get(self._client.BASE_URL + url, params=params, **kwargs)
 
+    def search_organizations(self, params=None, **kwargs):
+        url = 'organizations/search'
+        return self._client._get(self._client.BASE_URL + url, params=params, **kwargs)
+
+    #Deprecated, rather look at search_organizations
     def find_organization_by_name(self, **kwargs):
         url = 'organizations/find'
         return self._client._get(self._client.BASE_URL + url, **kwargs)
