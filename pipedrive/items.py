c603c8fb95cee88a34c4e0b11b9f55df50d3a94e
Danie Krige <dk@bwsonline.co.za>
2021-09-08 17:15:07 +0200

    Fixed Params

--- pipedrive/items.py
+++ pipedrive/items.py
@@ -2,6 +2,6 @@ class Items(object):
     def __init__(self, client):
         self._client = client
 
-    def get_item_search(self, **kwargs):
+    def get_item_search(self, params=None, **kwargs):
         url = 'itemSearch'
         return self._client._get(self._client.BASE_URL + url, **kwargs)
