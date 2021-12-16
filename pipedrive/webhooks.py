4cfcee3fe0adb47446754f8254bf27552930603a
sgiraldo <sgiraldo@gearplug.io>
2021-12-16 18:15:39 -0500

    fix: file format and webhook endpoints

--- pipedrive/webhooks.py
+++ pipedrive/webhooks.py
@@ -3,19 +3,18 @@ class Webhooks(object):
         self._client = client
 
     def get_hooks_subscription(self, **kwargs):
-        url = 'webhooks'
+        url = "webhooks"
         return self._client._get(url, **kwargs)
 
     def create_hook_subscription(self, subscription_url, event_action, event_object, **kwargs):
-        url = 'webhooks'
+        url = "webhooks"
         data = {
-            'subscription_url':
-                subscription_url,
-            'event_action': event_action,
-            'event_object': event_object
+            "subscription_url": subscription_url,
+            "event_action": event_action,
+            "event_object": event_object,
         }
-        return self._client._post(url, json=data, **kwargs)
+        return self._client._post(self._client.BASE_URL + url, json=data, **kwargs)
 
     def delete_hook_subscription(self, hook_id, **kwargs):
-        url = 'webhooks/{}'.format(hook_id)
-        return self._client._delete(url, **kwargs)
+        url = "webhooks/{}".format(hook_id)
+        return self._client._delete(self._client.BASE_URL + url, **kwargs)
