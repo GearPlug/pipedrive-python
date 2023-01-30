273b61bb65a3de325fe46dcbb651ea88444bef57
Miguel Ferrer <ingferrermiguel@gmail.com>
2023-01-30 09:14:13 -0500

    bump ver

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="pipedrive-python-lib",
-    version="1.2.0",
+    version="1.2.1",
     description="API wrapper for Pipedrive written in Python",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
